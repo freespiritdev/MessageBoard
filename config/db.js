e5a3a310aec4b9e0c4077177fef6523d3928ea4f
Cheri <avyishi@gmail.com>
2016-08-06 11:10:48 -0700

    Updated db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,6 @@
 const mysql = require('mysql');
-const error = require('chalk').white.bgRed.bold;
 
-if(!process.env.MYSQL_PASSWORD) {
-  throw error('Missing environment variable: MYSQL_PASSWORD')
-}
-
-const connection = mysql.createConnection({
+const connection = mysql.createConnection(process.env.JAWSDB_URL || {
   host: 'localhost',   
   user: 'root',
   password: process.env.MYSQL_PASSWORD,
